735d8ba1cdde11ea926fbcc478b62b346043ca8e
Yi Guo <yi.guo@yahoo.com>
2014-12-23 12:13:08 -0500

    Correct indentation

--- ctci/ch01/q1.py
+++ ctci/ch01/q1.py
@@ -3,28 +3,28 @@
 import sys
 
 def hasUniqueChar1(str):
-	if len(str) > 128:
-		return False
-	lst = list(sorted(str))
-	for i in range(len(lst) - 1):
-		if lst[i] == lst[i + 1]:
-			return False
-	return True 
+    if len(str) > 128:
+        return False
+    lst = list(sorted(str))
+    for i in range(len(lst) - 1):
+        if lst[i] == lst[i + 1]:
+            return False
+    return True 
 
 def hasUniqueChar2(str):
-	if len(str) > 128:
-		return False
-	lst = [False for i in range(128)]
-	for c in str:
-		if lst[ord(c)]:
-			return False
-		else:
-			lst[ord(c)] = True
-	return True
+    if len(str) > 128:
+        return False
+    lst = [False for i in range(128)]
+    for c in str:
+        if lst[ord(c)]:
+            return False
+        else:
+            lst[ord(c)] = True
+    return True
 
 def main():
-	print hasUniqueChar1(sys.argv[1])
-	print hasUniqueChar2(sys.argv[1])
+    print hasUniqueChar1(sys.argv[1])
+    print hasUniqueChar2(sys.argv[1])
 
 
 main()
