837103dc89d3d30766ef43c6f5bbcf0104142d77
Yi Guo <yi.guo@yahoo.com>
2014-12-23 12:10:03 -0500

    Convert indentation to tabs

--- ctci/ch01/q3.py
+++ ctci/ch01/q3.py
@@ -3,27 +3,27 @@
 import sys
 
 def isPermutation1(str1, str2):
-    if len(str1) != len(str2):
-        return False
-    lst1 = sorted(str1)
-    lst2 = sorted(str2)
-    for i in range(len(str1)):
-        if lst1[i] != lst2[i]:
-            return False
-    return True
+	if len(str1) != len(str2):
+		return False
+	lst1 = sorted(str1)
+	lst2 = sorted(str2)
+	for i in range(len(str1)):
+		if lst1[i] != lst2[i]:
+			return False
+	return True
 
 def isPermutation2(str1, str2):
-    if len(str1) != len(str2):
-        return False
-    lst = [0 for i in range(128)]
-    for i in range(len(str1)):
-        lst[ord(str1[i])] = lst[ord(str1[i])] + 1
-        lst[ord(str2[i])] = lst[ord(str2[i])] - 1
-    return not all(lst)
+	if len(str1) != len(str2):
+		return False
+	lst = [0 for i in range(128)]
+	for i in range(len(str1)):
+		lst[ord(str1[i])] = lst[ord(str1[i])] + 1
+		lst[ord(str2[i])] = lst[ord(str2[i])] - 1
+	return not all(lst)
 
 def main():
-    print isPermutation1(sys.argv[1], sys.argv[2])
-    print isPermutation2(sys.argv[1], sys.argv[2])
+	print isPermutation1(sys.argv[1], sys.argv[2])
+	print isPermutation2(sys.argv[1], sys.argv[2])
 
 
 main()
