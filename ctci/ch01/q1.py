3c013abb789d6b8810d1cc5231a279994303ad13
Yi Guo <yi.guo@yahoo.com>
2014-12-26 17:20:49 -0500

    Add a solution to q1

--- ctci/ch01/q1.py
+++ ctci/ch01/q1.py
@@ -2,15 +2,19 @@
 
 import sys
 
+# Sort the string and check neighboring chars
+# O(nlog(n))
 def hasUniqueChar1(str):
     if len(str) > 128:
         return False
-    lst = list(sorted(str))
+    lst = sorted(str)
     for i in range(len(lst) - 1):
         if lst[i] == lst[i + 1]:
             return False
     return True 
 
+# Construct a hash table for all ASCII chars
+# O(n)
 def hasUniqueChar2(str):
     if len(str) > 128:
         return False
@@ -22,9 +26,17 @@ def hasUniqueChar2(str):
             lst[ord(c)] = True
     return True
 
+# Construct a hash set of all chars in the string and compare the length
+# O(n)
+def hasUniqueChar3(str):
+    if (len(str)) > 128:
+        return False
+    return len(set(str)) == len(str)
+
 def main():
     print hasUniqueChar1(sys.argv[1])
     print hasUniqueChar2(sys.argv[1])
+    print hasUniqueChar3(sys.argv[1])
 
 
 main()
