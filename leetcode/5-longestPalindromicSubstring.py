4af9cdef732fcbe13722da76b7ad1cf14f92e352
Yi Guo <yi.guo@yahoo.com>
2015-01-01 13:19:48 -0500

    Start the longest palindromic substring problem

---
+++ leetcode/5-longestPalindromicSubstring.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic subtring.
+
+def longestPalindromicSubstring(s):
+    if len(s) < 2:
+        return s
+    for i in range(len(s)):
+        l, r = i - 1, i + 1
+
+def isPalindromic(S):
+    if len(S) < 2:
+        return True
+    else:
+        return S[0] == S[len(S) - 1] and isPalindromic(S[1:len(S) - 1])
+
+def main():
+    print longestPalindromicSubstring("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth")
+
+main()
