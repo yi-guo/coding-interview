8d1b8b7398e7e5ebff9aa086b06e7c1e5f473bce
Yi Guo <yi-guo@users.noreply.github.com>
2015-01-01 18:46:20 -0500

    Update 5-longestPalindromicSubstring.py

--- leetcode/5-longestPalindromicSubstring.py
+++ leetcode/5-longestPalindromicSubstring.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-# Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic subtring.
+# Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000,
+# and there exists one unique longest palindromic subtring.
 
 def longestPalindromicSubstring(s):
     if len(s) < 2:
