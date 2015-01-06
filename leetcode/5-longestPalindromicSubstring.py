d8e1aecdd6d3035dcd5bdddeecf599cdd5173e90
yi-guo <yi.guo@yahoo.com>
2015-01-06 06:09:35 -0500

    Complete the longest palindromic subtring problem

--- leetcode/5-longestPalindromicSubstring.py
+++ leetcode/5-longestPalindromicSubstring.py
@@ -3,19 +3,30 @@
 # Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000,
 # and there exists one unique longest palindromic subtring.
 
+# For each character, find the longest palindrom centered at itself. O(n^2)
 def longestPalindromicSubstring(s):
     if len(s) < 2:
         return s
+    longest = str()
     for i in range(len(s)):
-        l, r = i - 1, i + 1
+        p1, p2 = longestPalindrom(s, i - 1, i + 1), str()
+        if i < len(s) - 1 and s[i] == s[i + 1]:
+            p2 = longestPalindrom(s, i - 1, i + 2)
+        longest = max(p1, p2, longest, key = len)
+    return longest
 
-def isPalindromic(S):
-    if len(S) < 2:
-        return True
-    else:
-        return S[0] == S[len(S) - 1] and isPalindromic(S[1:len(S) - 1])
+# Given a character, keep expanding until its left and right are different.
+def longestPalindrom(s, l, r):
+    while l >= 0 and r < len(s):
+        if s[l] != s[r]:
+            break
+        l = l - 1
+        r = r + 1
+    return s[l + 1 : r]
+
 
 def main():
-    print longestPalindromicSubstring("civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth")
+   s = "12343343321"
+   print longestPalindromicSubstring(s)
 
 main()
