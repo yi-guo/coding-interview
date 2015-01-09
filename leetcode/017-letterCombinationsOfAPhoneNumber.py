c42865557a15db8a7e5c655c803e5d5a2d9efd1d
Yi Guo <yi.guo@yahoo.com>
2015-01-09 04:18:35 -0500

    Complete the letter combinations of a phone number problem

---
+++ leetcode/017-letterCombinationsOfAPhoneNumber.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+# Given a digit string, return all possible letter combinations that the number could represent.
+
+# Input: Digit string "23"
+# Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+
+# Note: Although the above answer is in lexicographical order, your answer could be in any order you want.
+
+# Depth-first search
+def letterCombinations(digits):
+    table = {'2' : 'abc', '3' : 'def',  '4' : 'ghi', '5' : 'jkl',
+             '6' : 'mno', '7' : 'pqrs', '8' : 'tuv', '9' : 'wxyz'}
+    i, combinations = 0, [str()]
+    while i < len(digits):
+        if digits[i] in table:
+            j, length = 0, len(combinations)
+            while j < length:
+                combo = combinations.pop(0)
+                for c in table[digits[i]]:
+                    combinations.append(combo + c)
+                j = j + 1
+        i = i + 1
+    return combinations
+
+def main():
+    print letterCombinations('23')
+
+main()
