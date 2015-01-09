71849a67cc5dd80e8f300727a8fc5183cec29753
Yi Guo <yi.guo@yahoo.com>
2015-01-09 12:43:54 -0500

    Complete the swap nodes in pairs problem

---
+++ leetcode/024-swapNodesInPairs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+# Given a linked list, swap every two adjacent nodes and return its head.
+
+# For example, given 1 -> 2 -> 3 -> 4, you should return the list as 2 -> 1 -> 4 -> 3.
+
+# Your algorithm should use only constant space. You may not modify the values in the list.
+# Only nodes itself can be changed.
+
+from LinkedList import Node, LinkedList, display
+
+# Recursively traverse to the end of the list and start swapping backward.
+# T(n) = T(n-2) + O(1), thus O(n).
+def swapPairs(head):
+    if not head or not head.next:
+        return head
+    else:
+        rest = swapPairs(head.next.next)
+        temp = head.next
+        temp.next = head
+        head.next = rest
+        return temp
+
+def main():
+    print display(swapPairs(LinkedList([1, 2, 3, 4]).head))
+
+main()
