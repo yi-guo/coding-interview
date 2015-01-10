915b7b29864b585b238dbd1e535a7de1e36fd53d
Yi Guo <yi.guo@yahoo.com>
2015-01-10 07:42:30 -0500

    Complete the implementation of strStr

--- leetcode/028-implement-strStr.py
+++ leetcode/028-implement-strStr.py
@@ -4,30 +4,31 @@
 
 # Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
+# For each character in haystack, if it matches the first character of needle, start checking the rest.
+# Do so until there are fewer characters left in haystack than needle.
+# Since each character in haystack could possibly be visited twice, the algorithm takes O(n^2).
+# This solution passes the OJ; however, check out KMP algorithm for smarter solution, which completes in O(n)!
 def strStr(haystack, needle):
-    index = -1
-    i = j = 0
-    while i < len(haystack):
+    if len(haystack) < len(needle):
+        return -1
+    elif not needle or haystack == needle:
+        return 0
+    i, index = 0, -1
+    while i < len(haystack) - len(needle) + 1:
         if haystack[i] == needle[0]:
-            index = i
-            print index
-            for j in range(len(needle)):
-                print i, j
-                if haystack[i] != needle[j]:
+            index, j = i, i + 1
+            for k in range(1, len(needle)):
+                if haystack[j] == needle[k]:
+                    j = j + 1
+                else:
                     index = -1
                     break
-                else:
-                    i = i + 1
             if index != -1:
                 return index
         i = i + 1
     return index
 
-
-
-
-
 def main():
-    print strStr("ABC ABCDAB ABCDABCDABDE", "ABCDABD")
+    print strStr("ABC ABCDAB ABCDABCDABD", "ABCDABD")
 
 main()
