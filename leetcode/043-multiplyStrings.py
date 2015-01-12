c077d73357989a7eb6073161ea49cc7bd444dcc6
Yi Guo <yi.guo@yahoo.com>
2015-01-12 15:11:15 -0500

    Complete the multiply strings problem

---
+++ leetcode/043-multiplyStrings.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+# Given two numbers represented as strings, return multiplication of the numbers as a string.
+
+# Note: The numbers can be arbitrarily large and are non-negative.
+
+def multiply(num1, num2):
+    if num1 == '0' or num2 == '0':
+        return '0'
+    elif num1 == '1':
+        return num2
+    elif num2 == '1':
+        return num1
+    product = [0] * (len(num1) + len(num2))
+    for i, n2 in enumerate(num2):
+        for j, n1 in enumerate(num1):
+            product[i + j + 1] += int(n1) * int(n2)
+    carry = 0
+    for i in range(len(product) - 1, -1, -1):
+        product[i] += carry
+        num = str(product[i])
+        if len(num) > 1:
+            carry = int(num[:len(num) - 1])
+            product[i] = int(num[len(num) - 1])
+        else:
+            carry = 0
+    if product[0] == 0:
+        return ''.join(str(n) for n in product[1:])
+    else:
+        return ''.join(str(n) for n in product)
+
+def main():
+    print multiply('999', '999')
+
+
+main()
