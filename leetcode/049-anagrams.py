247e5037696d697d6e336d387024eebd66c79e3f
Yi Guo <yi.guo@yahoo.com>
2015-01-12 20:43:15 -0500

    Complete the anagrams problem

---
+++ leetcode/049-anagrams.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+# Given an array of strings, return all groups of strings that are anagrams.
+
+# Note: All inputs will be in lower-case.
+
+# For every given string, use the sorted string as a key to group anagrams together in a hash table.
+def anagrams(strs):
+    if not strs or len(strs) < 2:
+        return list()
+    hashmap = dict()
+    for s in strs:
+        key = ''.join(sorted(s))
+        if key in hashmap:
+            hashmap[key].append(s)
+        else:
+            hashmap[key] = [s]
+    anagrams = list()
+    for key in hashmap:
+        if len(hashmap[key]) > 1:
+            anagrams.extend(hashmap[key])
+    return anagrams
+
+def main():
+    print anagrams(["cab","pug","pei","nay","ron","rae","ems","ida","mes"])
+
+main()
