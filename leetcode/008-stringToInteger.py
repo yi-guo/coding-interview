6c61c425eed75b1b7815b8b76c069db7108e41ff
Yi Guo <yi.guo@yahoo.com>
2015-01-29 13:52:54 -0500

    Optimize source code

--- leetcode/008-stringToInteger.py
+++ leetcode/008-stringToInteger.py
@@ -8,7 +8,7 @@
 # Notes: It is intended for this problem to be specified vaguely (i.e., no given input specs). You are responsible
 # to gather all the input requirements up front.
 
-def atoi(str):
+def atoi1(str):
     if not str:
         return 0
     INT_MIN = -2147483648
@@ -38,7 +38,25 @@ def atoi(str):
     else:
         return num
 
+def atoi2(str):
+    str = str.strip()
+    if not str: return 0
+    neg = 1
+    i = num = 0
+    if str[i] == '+' or str[i] == '-':
+        neg = -1 if str[i] == '-' else 1
+        i = i + 1
+    while i < len(str):
+        if not str[i].isdigit():
+            break
+        num = num * 10 + ord(str[i]) - ord('0')
+        i = i + 1
+    return max(min(num * neg, 2147483647), -2147483648)
+
+
+
 def main():
-    print atoi(' \t -003073-\nab42')
+    print atoi1(' \t -003073-\nab42')
+    print atoi1(' \t -003073-\nab42')
 
 main()
