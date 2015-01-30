f2e857caf3360faa1854f616ebbfbdf621c6ccf8
Yi Guo <yi.guo@yahoo.com>
2015-01-30 10:42:47 -0500

    Update solution without the use of string or list

--- leetcode/009-palindromeNumber.py
+++ leetcode/009-palindromeNumber.py
@@ -2,26 +2,25 @@
 
 # Determine whether an integer is a palindrome. Do this without extra space.
 
+
 def isPalindrome(x):
     if x < 0:
         return False
-    if x < 10:
-        return True
-    elif x < 100:
-        return x % 11 == 0
-    n = 3
-    while x - pow(10, n) >= 0:
-        n = n + 1
+    div = 1
+    while x / div >= 10:
+        div *= 10
     while x != 0:
+        ms = x / div
         ls = x % 10
-        ms = x / pow(10, n - 1)
-        if ls != ms:
+        if ms != ls:
             return False
-        x = x % pow(10, n - 1) / 10
-        n = n - 2
+        x = x % div / 10
+        div /= 100
     return True
+
 
 def main():
     print isPalindrome(1000021)
+
 
 main()
