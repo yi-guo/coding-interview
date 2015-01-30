ab87c06ba77f70174f3e32132df13c6f61612ac1
Yi Guo <yi.guo@yahoo.com>
2015-01-30 16:20:34 -0500

    Optimize Q70

--- leetcode/022-generateParentheses.py
+++ leetcode/022-generateParentheses.py
@@ -5,31 +5,29 @@
 # For example, given n = 3, a solution set is "((()))", "(()())", "(())()", "()(())", "()()()".
 
 import sys
+import collections
+
 
 # For every parenthesis of what we have, make new ones by inserting another pair after every possible '('.
 # Also append another pair to every parenthesis of what we have.
 # Eliminate duplicates using a hash set.
 def generateParenthesis(n):
-    if n < 1:
-        return [str()]
-    elif n == 1:
-        return ['()']
-    exists, parentheses = set(), [str()]
-    for i in range(n):
-        j, length = 0, len(parentheses)
-        while j < length:
-            head = parentheses.pop(0)
-            if head + '()' not in exists:
-                exists.add(head + '()')
-                parentheses.append(head + '()')
-            for k in range(len(head)):
-                if head[k] == '(':
-                    new = head[:k + 1] + '()' + head[k + 1:]
-                    if new not in exists:
-                        exists.add(new)
-                        parentheses.append(new)
-            j = j + 1
-    return parentheses
+    if n < 1: return list()
+    existed = set()
+    queue = collections.deque(['()'])
+    while len(queue[0]) < n * 2:
+        curr = queue.popleft()
+        new = curr + '()'
+        if new not in existed:
+            queue.append(new)
+            existed.add(new)
+        for i in xrange(len(curr)):
+            if curr[i] == '(':
+                new = curr[:i+1] + '()' + curr[i+1:]
+                if new not in existed:
+                    queue.append(new)
+                    existed.add(new)
+    return list(queue)
 
 def main():
     print generateParenthesis(int(sys.argv[1]))
