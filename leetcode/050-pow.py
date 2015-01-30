316c473576709fbc76e8f9d450c91f9148377934
Yi Guo <yi.guo@yahoo.com>
2015-01-30 16:10:41 -0500

    Optimize Q50

--- leetcode/050-pow.py
+++ leetcode/050-pow.py
@@ -2,25 +2,21 @@
 
 # Implement pow(x, n)
 
+
 # x^n = x^(n/2) * x^(n/2) * x^(n%2). Pay attention to n < 0.
 def pow(x, n):
-    if x == 0 or x == 1:
-        return x
     if n == 0:
         return 1
     elif n == 1:
         return x
+    elif n < 0:
+        return pow(1 / x, abs(n))
     else:
-        sqrt = pow(x, abs(n) / 2)
-        if n % 2 != 0:
-            if n < 0:
-                return 1 / float(sqrt * sqrt * pow(x, abs(n) % 2))
-            return sqrt * sqrt * pow(x, abs(n) % 2)
-        if n < 0:
-            return 1 / float(sqrt * sqrt)
-        return sqrt * sqrt 
+        halfPow = pow(x, n / 2)
+        return halfPow * halfPow if n % 2 == 0 else halfPow * halfPow * x
+
 
 def main():
-    print pow(2, -3)
+    print pow(2.0, -3)
 
 main()
