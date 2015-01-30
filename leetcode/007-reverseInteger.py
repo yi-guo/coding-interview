1e8931d9669361dcd5bc25f052a23d16c05107c0
Yi Guo <yi.guo@yahoo.com>
2015-01-30 10:21:31 -0500

    Update solution without the use of string or list

--- leetcode/007-reverseInteger.py
+++ leetcode/007-reverseInteger.py
@@ -7,25 +7,24 @@
 
 # For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
 
+
 # Convert the integer into a list of digits and reverse
 def reverse(x):
-    if abs(x) < 10:
-        return x
-    l = [i for i in str(abs(x))]
-    i, j = 0, len(l) - 1
-    while i < j:
-        l[i], l[j] = l[j], l[i]
-        i = i + 1
-        j = j - 1
-    r = int(''.join(l))
-    if x < 0:
-        r = 0 - r
-    if r < -2147483648 or r > 2147483647:
+    neg = -1 if x < 0 else 1
+    x, num = abs(x), 0
+    while x != 0:
+        digit = x % 10
+        num = num * 10 + digit
+        x /= 10
+    num *= neg
+    if num < -2147483648 or num > 2147483647:
         return 0
-    return r
+    return num
+
 
 def main():
-    x = 1000000003
+    x = 100003
     print reverse(x)
+
 
 main()
