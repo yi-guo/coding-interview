54d2f1d09908d0c655f1a5ab36d9e8d7d4ae1ffa
Yi Guo <yi.guo@yahoo.com>
2015-01-30 16:18:02 -0500

    Optimize Q70

--- leetcode/070-climbingStairs.py
+++ leetcode/070-climbingStairs.py
@@ -8,14 +8,14 @@ import sys
 
 # Dynamic programming. O(n).
 def climbStairs(n):
-    if n == 1:
+    if n < 1:
+        return 0
+    elif n == 1:
         return 1
-    elif n == 2:
-        return 2
     d = [1, 2]
-    for i in range(2, n):
+    for i in xrange(2, n):
         d.append(d[i - 1] + d[i - 2])
-    return d[len(d) - 1]
+    return d[-1]
 
 def main():
     print climbStairs(int(sys.argv[1]))
