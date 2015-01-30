336153a8bb9247a4027916ca40b07a439f7e2a7b
Yi Guo <yi.guo@yahoo.com>
2015-01-30 16:34:47 -0500

    Optimize Q21

--- leetcode/021-mergeTwoSortedLists.py
+++ leetcode/021-mergeTwoSortedLists.py
@@ -3,19 +3,12 @@
 # Merge two sorted linked lists and return it as a new list.
 # The new list should be made by splicing together the nodes of the first two lists.
 
+from LinkedList import Node
+
+
 # A straight forward solution that compares while moving foward. O(n)
 def mergeTwoLists(l1, l2):
-    if not l1:
-        return l2
-    elif not l2:
-        return l1
-    head = None
-    if l1.val < l2.val:
-        head = l1
-        l1 = l1.next
-    else:
-        head = l2
-        l2 = l2.next
+    head = Node(0)
     temp = head
     while l1 and l2:
         if l1.val < l2.val:
@@ -27,8 +20,8 @@ def mergeTwoLists(l1, l2):
         temp = temp.next
     if l1:
         temp.next = l1
-    elif l2:
+    if l2:
         temp.next = l2
-    return head
+    return head.next
 
 # Mah, do I need test cases for such an easy problem?
