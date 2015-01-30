b6b66cd7f0eb881223c9602343f989da4a0755b3
Yi Guo <yi.guo@yahoo.com>
2015-01-30 16:31:32 -0500

    Optimize Q15

--- leetcode/017-letterCombinationsOfAPhoneNumber.py
+++ leetcode/017-letterCombinationsOfAPhoneNumber.py
@@ -7,10 +7,11 @@
 
 # Note: Although the above answer is in lexicographical order, your answer could be in any order you want.
 
+
 # Depth-first search
 def letterCombinations(digits):
-    table = {'2' : 'abc', '3' : 'def',  '4' : 'ghi', '5' : 'jkl',
-             '6' : 'mno', '7' : 'pqrs', '8' : 'tuv', '9' : 'wxyz'}
+    table = {'2': 'abc', '3': 'def',  '4': 'ghi', '5': 'jkl',
+             '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
     i, combinations = 0, [str()]
     while i < len(digits):
         if digits[i] in table:
