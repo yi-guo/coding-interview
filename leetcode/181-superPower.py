8ff71d34400e352bbbc91e2aa1127d2d89a3b4aa
Yi Guo <yi.guo@yahoo.com>
2015-02-01 03:44:45 -0500

    Add Q181 & Q182 from HackerRank

---
+++ leetcode/181-superPower.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+# Given a positive integer z, where 1 <= z <= 10^9, check if z can be written as p ^ q, where p and q are positive
+# integers greater than 1. If z can be written as p ^ q, return 1. Otherwise, return 0.
+
+
+import sys
+
+
+# Binary search.
+def superPower(z):
+    if z < 4:
+        return 0
+    for q in xrange(2, 30):
+        i, j = 2, z / q
+        while i <= j:
+            p = (i + j) / 2
+            power = pow(p, q)
+            if power < z:
+                i = p + 1
+            elif power > z:
+                j = p - 1
+            else:
+                return 1
+    return 0
+
+
+def main():
+    print superPower(int(sys.argv[1]))
+
+
+main()
