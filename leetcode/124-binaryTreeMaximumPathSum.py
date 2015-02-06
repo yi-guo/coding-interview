bfdfee92fa01f942a382eca43777b450b25da4e4
Yi Guo <yi.guo@yahoo.com>
2015-02-05 21:55:55 -0500

    Complete the binary tree maximum path sum problem

--- leetcode/124-binaryTreeMaximumPathSum.py
+++ leetcode/124-binaryTreeMaximumPathSum.py
@@ -10,7 +10,31 @@
 #     2   3
 # return 6.
 
+from Tree import Tree
+
+# Recursively compute the maximum path sum for the subtrees.
+# Maximum path sum occurs in the following four cases only:
+#   1. current node
+#   2. current node + left maximum path sum
+#   3. current node + right maximum path sum
+#   4. current node + left and right maximum path sum
 def maxPathSum(root):
+    pathSum = [float('-inf')]
+    maxStraightPathSum(root, pathSum)
+    return pathSum[0]
+
+def maxStraightPathSum(root, pathSum):
     if not root:
         return 0
-    
+    left = maxStraightPathSum(root.left, pathSum)
+    right = maxStraightPathSum(root.right, pathSum)
+    arch = left + right + root.val
+    straight = max(root.val, max(left, right) + root.val)
+    pathSum[0] = max(pathSum[0], arch, straight)
+    return straight
+
+def main():
+    tree = Tree([-1, -2, -5, -3, 3000, '#', '#', '#', '#', 2000, -3000])
+    print maxPathSum(tree.root)
+
+main()
