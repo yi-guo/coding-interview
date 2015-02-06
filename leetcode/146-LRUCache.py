202153902cf3f7f8a1d55fbd2fae07838358d123
Yi Guo <yi.guo@yahoo.com>
2015-02-05 21:07:53 -0500

    Complete the LRU cache problem

--- leetcode/146-LRUCache.py
+++ leetcode/146-LRUCache.py
@@ -9,14 +9,23 @@
 # Define ListNode for doubly linked list
 class ListNode:
 
-    def __init__(self, val):
+    def __init__(self, key, val):
+        self.key = key
         self.val = val
         self.prev = None
         self.next = None
 
+    def __repr__(self):
+        ret, temp = '[%d' % self.val, self.next
+        while temp:
+            ret += ', %d' % temp.val
+            temp = temp.next
+        return ret + ']'
+
 # Define LRUCache
 class LRUCache:
 
+    # Doubly linked list and hash table.
     def __init__(self, capacity):
         self.size = 0
         self.head = None
@@ -27,12 +36,16 @@ class LRUCache:
     def get(self, key):
         if key in self.cache:
             node = self.cache[key]
-            if node.next:
-                node.next.prev = node.prev
-            if node.prev:
+            if node == self.tail:
+                return node.val
+            node.next.prev = node.prev
+            if node == self.head:
+                self.head = self.head.next
+            else:
                 node.prev.next = node.next
             self.tail.next = node
             node.prev = self.tail
+            node.next = None
             self.tail = node
             return node.val
         return -1
@@ -40,7 +53,7 @@ class LRUCache:
     def set(self, key, value):
         if self.get(key) == -1:
             if self.size < self.capacity:
-                node = ListNode(value)
+                node = ListNode(key, value)
                 if self.size != 0:
                     self.tail.next = node
                     node.prev = self.tail
@@ -50,8 +63,28 @@ class LRUCache:
                 self.cache[key] = node
                 self.size = self.size + 1
             else:
-
-
+                head = self.head
+                del self.cache[head.key]
+                if self.head.next:
+                    self.head = head.next
+                    self.head.prev = None
+                head.key = key
+                head.val = value
+                self.cache[key] = head
+                self.tail.next = head
+                head.prev = self.tail
+                head.next = None
+                self.tail = head
         else:
             self.cache[key].val = value
 
+def main():
+    cache = LRUCache(2)
+    cache.set(2, 1)
+    cache.set(3, 2)
+    print cache.get(3)
+    print cache.get(2)
+    cache.set(4, 3)
+    print cache.get(2)
+
+main()
