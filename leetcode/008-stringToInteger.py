c6335a3d6268ee32a34b07fe1cca7597fbf448ec
Yi Guo <yi.guo@yahoo.com>
2015-02-08 16:03:25 -0500

    Optimize Q8

--- leetcode/008-stringToInteger.py
+++ leetcode/008-stringToInteger.py
@@ -9,19 +9,17 @@
 # to gather all the input requirements up front.
 
 def atoi(str):
-    str = str.strip()
+    str = str.lstrip()
     if not str: return 0
-    neg = 1
-    i = num = 0
-    if str[i] == '+' or str[i] == '-':
-        neg = -1 if str[i] == '-' else 1
-        i = i + 1
-    while i < len(str):
-        if not str[i].isdigit():
+    neg, num = 1, 0
+    if str[0] == '+' or str[0] == '-':
+        neg = -1 if str[0] == '-' else 1
+        str = str[1:]
+    for c in str:
+        if not c.isdigit():
             break
-        num = num * 10 + ord(str[i]) - ord('0')
-        i = i + 1
-    return max(min(num * neg, 2147483647), -2147483648)
+        num = num * 10 + ord(c) - ord('0')
+    return max(min(neg * num, 2147483647), -2147483648)
 
 def main():
     print atoi(' \t -003073-\nab42')
