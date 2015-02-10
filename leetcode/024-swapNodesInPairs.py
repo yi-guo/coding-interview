4338a816faf5c48b76ec4b3da42cedad08243a6b
Yi Guo <yi.guo@yahoo.com>
2015-02-10 01:41:31 -0500

    Add another solution to Q24

--- leetcode/024-swapNodesInPairs.py
+++ leetcode/024-swapNodesInPairs.py
@@ -7,11 +7,11 @@
 # Your algorithm should use only constant space. You may not modify the values in the list.
 # Only nodes itself can be changed.
 
-from LinkedList import LinkedList
+from LinkedList import ListNode, LinkedList
 
 # Recursively traverse to the end of the list and start swapping backward.
 # T(n) = T(n-2) + O(1), thus O(n).
-def swapPairs(head):
+def swapPairs1(head):
     if not head or not head.next:
         return head
     else:
@@ -20,6 +20,20 @@ def swapPairs(head):
         temp.next = head
         head.next = rest
         return temp
+
+# Iterative approach. One pass, thus O(n).
+def swapPairs2(head):
+    node = ListNode(0)
+    node.next = head
+    prev, curr = node, head
+    while curr and curr.next:
+        temp = curr.next.next
+        curr.next.next = prev.next
+        prev.next = curr.next
+        curr.next = temp
+        prev = curr
+        curr = prev.next
+    return node.next
 
 def main():
     print swapPairs(LinkedList([1, 2, 3, 4]).head)
