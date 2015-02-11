acd4e54c8e04eb817fb092aadf81eed66ba1fa76
Yi Guo <yi.guo@yahoo.com>
2015-02-11 02:06:25 -0500

    Optimize solutions

--- leetcode/091-decodeWays.py
+++ leetcode/091-decodeWays.py
@@ -17,22 +17,12 @@ import sys
 
 # Dynamic programming where i is the number of ways decoding s[i:]. One pass, thus O(n).
 def numDecodings(s):
-    if not s: return 0          # s is empty, thus 0
-    if s[0] == '0': return 0    # s starts with 0, thus 0
-    if len(s) == 1: return 1    # s is of length 1, thus 1 since it's already not 0
-    # Build DP where the base case says if the last element in s is not 0, then there is only 1 way to decode.
-    d = [1 for i in range(len(s) + 1)]
-    d[-2] = 1 if s[-1] != '0' else 0
-    # Maintain DP. Be careful with 0s.
-    for i in range(len(d) - 3, -1, -1):
-        # If s[i] is 0, then d[i] is d[i + 1] if s[i - 1] is either 1 or 2; otherwise, d[i] = 0
-        if s[i] == '0':
-            d[i] = d[i + 1] if d[i - 1] == '1' or d[i - 1] == '2' else 0
-        # If not otherwise, d[i] = the number of ways to decode s[i + 1:] and the number of ways to decode s[i + 2:]
-        # given s[i : i + 2] can be decoded.
-        else:
-            d[i] = d[i + 1] + int(int(s[i : i + 2]) < 27) * d[i + 2]
-    return d[0]
+    if not s or s[0] == '0':
+        return 0
+    decodings = [1, 1]
+    for i in xrange(1, len(s)):
+        decodings.append(int(s[i] != '0') * decodings[i] + int(s[i - 1] != '0' and 0 < int(s[i - 1 : i + 1]) < 27) * decodings[i - 1])
+    return decodings[len(s)]
 
 def main():
     print numDecodings(sys.argv[1])
