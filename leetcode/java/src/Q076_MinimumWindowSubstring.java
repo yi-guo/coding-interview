37c37d0a0850b77f582bcec8b2436eec3ca4d36e
Yi Guo <yi.guo@yahoo.com>
2015-03-27 23:50:31 -0700

    Modify/Add Q19, Q20, Q21, Q141, and Q142

--- leetcode/java/src/Q076_MinimumWindowSubstring.java
+++ leetcode/java/src/Q076_MinimumWindowSubstring.java
@@ -3,7 +3,8 @@ import java.util.HashMap;
 public class Q076_MinimumWindowSubstring {
 
     /**
-     * Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+     * Given a string S and a string T, find the minimum window in S which will contain all the characters
+     * in T in complexity O(n).
      *
      * For example,
      *   S = "ADOBECODEBANC"
@@ -12,7 +13,8 @@ public class Q076_MinimumWindowSubstring {
      *
      * Note:
      * 1. If there is no such window in S that covers all characters in T, return the empty string "".
-     * 2. If there are multiple such windows, you are guaranteed that there will always be only one unique minimum window in S.
+     * 2. If there are multiple such windows, you are guaranteed that there will always be only one unique
+     *    minimum window in S.
      *
      */
 
